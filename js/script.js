b95e4aa831599609e11ec7a076b1375dc1d0b71f
Jasmina123-ask <jasminaan2005@gmail.com>
2025-10-10 14:55:30 +0300

    referencement

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ document.addEventListener('DOMContentLoaded', function() {
         navMenu.classList.toggle('active');
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => { 
+    const form = document.getElementById("contactForm"); 
+    const confirmation = document.getElementById("confirmationMessage"); 
+    
+    form.addEventListener("submit", (e) => { 
+        e.preventDefault(); // Empêche le rechargement 
+        confirmation.style.display = "block"; 
+        form.reset(); // Vide les champs 
+        }); 
+    });
+
+
+    service_dgdf2cq
